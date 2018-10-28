8672b46bd2b78fa345f1df5e513f8e068002f010
Apex Liu <apex.liu@qq.com>
2018-10-29 03:55:22 +0800

    .

--- client/tp_assist_macos/src/csrc/ts_cfg.cpp
+++ client/tp_assist_macos/src/csrc/ts_cfg.cpp
@@ -46,6 +46,220 @@ bool TsCfg::_load(const ex_astr& str_json) {
 		return false;
 	}
 
+	ex_astr sel_name;
+	size_t i = 0;
+	ex_astr tmp;
+	
+	//===================================
+	// check ssh config
+	//===================================
+	
+	if (!m_root["ssh"].isObject()) {
+		EXLOGE("invalid config, error 1.\n");
+		return false;
+	}
+	
+	if (!m_root["ssh"]["selected"].isString()) {
+		EXLOGE("invalid config, error 2.\n");
+		return false;
+	}
+	
+	sel_name = m_root["ssh"]["selected"].asCString();
+	
+	if (!m_root["ssh"]["available"].isArray() || m_root["ssh"]["available"].size() == 0) {
+		EXLOGE("invalid config, error 3.\n");
+		return false;
+	}
+	
+	for (i = 0; i < m_root["ssh"]["available"].size(); ++i) {
+		
+		if (
+			!m_root["ssh"]["available"][i]["name"].isString()
+			|| !m_root["ssh"]["available"][i]["app"].isString()
+			|| !m_root["ssh"]["available"][i]["cmdline"].isString()
+			) {
+			EXLOGE("invalid config, error 4.\n");
+			return false;
+		}
+		
+		if (m_root["ssh"]["available"][i]["display"].isNull()) {
+			m_root["ssh"]["available"][i]["display"] = m_root["ssh"]["available"][i]["name"];
+		}
+		
+		if (m_root["ssh"]["available"][i]["name"].asCString() != sel_name)
+			continue;
+		
+		ssh_app = m_root["ssh"]["available"][i]["app"].asCString();
+		ssh_cmdline = m_root["ssh"]["available"][i]["cmdline"].asCString();
+		
+		break;
+	}
+	
+	if (ssh_app.length() == 0 || ssh_cmdline.length() == 0) {
+		EXLOGE("invalid config, error 6.\n");
+		return false;
+	}
+	
+	
+	//===================================
+	// check sftp config
+	//===================================
+	
+	if (!m_root["scp"].isObject()) {
+		EXLOGE("invalid config, error 1.\n");
+		return false;
+	}
+	
+	if (!m_root["scp"]["selected"].isString()) {
+		EXLOGE("invalid config, error 2.\n");
+		return false;
+	}
+	
+	sel_name = m_root["scp"]["selected"].asCString();
+	
+	if (!m_root["scp"]["available"].isArray() || m_root["scp"]["available"].size() == 0) {
+		EXLOGE("invalid config, error 3.\n");
+		return false;
+	}
+	
+	for (i = 0; i < m_root["sftp"]["available"].size(); ++i) {
+		
+		if (
+			!m_root["sftp"]["available"][i]["name"].isString()
+			|| !m_root["sftp"]["available"][i]["app"].isString()
+			|| !m_root["sftp"]["available"][i]["cmdline"].isString()
+			) {
+			EXLOGE("invalid config, error 4.\n");
+			return false;
+		}
+		
+		if (m_root["scp"]["available"][i]["display"].isNull()) {
+			m_root["scp"]["available"][i]["display"] = m_root["scp"]["available"][i]["name"];
+		}
+		
+		if (m_root["scp"]["available"][i]["name"].asCString() != sel_name)
+			continue;
+		
+		tmp = m_root["scp"]["available"][i]["app"].asCString();
+		ex_astr2wstr(tmp, scp_app, EX_CODEPAGE_UTF8);
+		tmp = m_root["scp"]["available"][i]["cmdline"].asCString();
+		ex_astr2wstr(tmp, scp_cmdline, EX_CODEPAGE_UTF8);
+		
+		break;
+	}
+	
+	if (scp_app.length() == 0 || scp_cmdline.length() == 0) {
+		EXLOGE("invalid config, error 6.\n");
+		return false;
+	}
+	
+	//===================================
+	// check telnet config
+	//===================================
+	
+	if (!m_root["telnet"].isObject()) {
+		EXLOGE("invalid config, error 1.\n");
+		return false;
+	}
+	
+	if (!m_root["telnet"]["selected"].isString()) {
+		EXLOGE("invalid config, error 2.\n");
+		return false;
+	}
+	
+	sel_name = m_root["telnet"]["selected"].asCString();
+	
+	if (!m_root["telnet"]["available"].isArray() || m_root["telnet"]["available"].size() == 0) {
+		EXLOGE("invalid config, error 3.\n");
+		return false;
+	}
+	
+	for (i = 0; i < m_root["telnet"]["available"].size(); ++i) {
+		
+		if (
+			!m_root["telnet"]["available"][i]["name"].isString()
+			|| !m_root["telnet"]["available"][i]["app"].isString()
+			|| !m_root["telnet"]["available"][i]["cmdline"].isString()
+			) {
+			EXLOGE("invalid config, error 4.\n");
+			return false;
+		}
+		
+		if (m_root["telnet"]["available"][i]["display"].isNull()) {
+			m_root["telnet"]["available"][i]["display"] = m_root["telnet"]["available"][i]["name"];
+		}
+		
+		if (m_root["telnet"]["available"][i]["name"].asCString() != sel_name)
+			continue;
+		
+		tmp = m_root["telnet"]["available"][i]["app"].asCString();
+		ex_astr2wstr(tmp, telnet_app, EX_CODEPAGE_UTF8);
+		tmp = m_root["telnet"]["available"][i]["cmdline"].asCString();
+		ex_astr2wstr(tmp, telnet_cmdline, EX_CODEPAGE_UTF8);
+		
+		break;
+	}
+	
+	if (telnet_app.length() == 0 || telnet_cmdline.length() == 0) {
+		EXLOGE("invalid config, error 6.\n");
+		return false;
+	}
+	
+	//===================================
+	// check rdp config
+	//===================================
+	
+	if (!m_root["rdp"].isObject()) {
+		EXLOGE("invalid config, error 1.\n");
+		return false;
+	}
+	
+	if (!m_root["rdp"]["selected"].isString()) {
+		EXLOGE("invalid config, error 2.\n");
+		return false;
+	}
+	
+	sel_name = m_root["rdp"]["selected"].asCString();
+	
+	if (!m_root["rdp"]["available"].isArray() || m_root["rdp"]["available"].size() == 0) {
+		EXLOGE("invalid config, error 3.\n");
+		return false;
+	}
+	
+	for (i = 0; i < m_root["rdp"]["available"].size(); ++i) {
+		
+		if (
+			!m_root["rdp"]["available"][i]["name"].isString()
+			|| !m_root["rdp"]["available"][i]["app"].isString()
+			|| !m_root["rdp"]["available"][i]["cmdline"].isString()
+			) {
+			EXLOGE("invalid config, error 4.\n");
+			return false;
+		}
+		
+		if (m_root["rdp"]["available"][i]["display"].isNull()) {
+			m_root["rdp"]["available"][i]["display"] = m_root["rdp"]["available"][i]["name"];
+		}
+		
+		if (m_root["rdp"]["available"][i]["name"].asCString() != sel_name)
+			continue;
+		
+		tmp = m_root["rdp"]["available"][i]["app"].asCString();
+		ex_astr2wstr(tmp, rdp_app, EX_CODEPAGE_UTF8);
+		tmp = m_root["rdp"]["available"][i]["cmdline"].asCString();
+		ex_astr2wstr(tmp, rdp_cmdline, EX_CODEPAGE_UTF8);
+		tmp = m_root["rdp"]["available"][i]["name"].asCString();
+		ex_astr2wstr(tmp, rdp_name, EX_CODEPAGE_UTF8);
+		
+		break;
+	}
+	
+	if (rdp_app.length() == 0 || rdp_cmdline.length() == 0 || rdp_name.length() == 0) {
+		EXLOGE("invalid config, error 6.\n");
+		return false;
+	}
+	
+#if 0
 	// ------------ term ---------------------
 	
 	if (!m_root["term"].isObject()) {
@@ -154,6 +368,7 @@ bool TsCfg::_load(const ex_astr& str_json) {
 		EXLOGE("invalid config, error 6.\n");
 		return false;
 	}
+#endif
 
 	return true;
 }
