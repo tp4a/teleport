73051854dd0875ada857343240ac5702cd2614a9
Apex Liu <apex.liu@qq.com>
2018-11-10 14:09:29 +0800

    .

--- build/builder/build-assist.py
+++ build/builder/build-assist.py
@@ -76,6 +76,10 @@ class BuilderWin(BuilderBase):
         utils.copy_file(os.path.join(env.root_path, 'out', 'client', ctx.bits_path, ctx.target_path), tmp_app_path, 'tp_assist.exe')
         utils.copy_file(os.path.join(env.root_path, 'client', 'tp_assist_win', 'cfg'), tmp_cfg_path, ('tp-assist.default.json', 'tp-assist.json'))
 
+        utils.copy_file(os.path.join(env.root_path, 'client', 'tp_assist_win', 'cfg'), tmp_cfg_path, 'cacert.cer')
+        utils.copy_file(os.path.join(env.root_path, 'client', 'tp_assist_win', 'cfg'), tmp_cfg_path, 'localhost.key')
+        utils.copy_file(os.path.join(env.root_path, 'client', 'tp_assist_win', 'cfg'), tmp_cfg_path, 'localhost.pem')
+
         utils.copy_ex(os.path.join(env.root_path, 'client', 'tp_assist_win'), tmp_app_path, 'site')
 
         utils.makedirs(os.path.join(tmp_app_path, 'tools', 'putty'))
